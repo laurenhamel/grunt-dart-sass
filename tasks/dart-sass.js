3c9928be163db3d66f64607dbc6064dc504d8a26
Lauren Hamel <lauren.hamel@outlook.com>
2019-01-24 17:28:34 -0500

    Fixes issue with writing to the console after running the grunt task

--- tasks/dart-sass.js
+++ tasks/dart-sass.js
@@ -9,26 +9,26 @@
 'use strict';
 
 module.exports = function (grunt) {
-  
+
   // Register Grunt multi-task.
-  grunt.registerMultiTask('dart-sass', 'Compile Sass to CSS via Dart Sass', async function () { 
-    
+  grunt.registerMultiTask('dart-sass', 'Compile Sass to CSS via Dart Sass', async function () {
+
     // Load dependencies.
     const sass = require('sass');
     const extend = require('extend');
     const path = require('path');
     const chalk = require('chalk');
     const intercept = require('intercept-stdout');
-    
+
     // Make asynchronous.
     const done = this.async();
-    
+
     // Set options.
-    const options = this.options(); 
-    
+    const options = this.options();
+
     // Initialize default logger type.
     let type = 'log';
-    
+
     // Define logger styles.
     const styles = {
       log: [],
@@ -40,16 +40,16 @@ module.exports = function (grunt) {
 
     // Initialize results.
     let results = [];
-    
+
     // Initialize counter.
     let compiled = 0;
-    
+
     // Initialize console interceptor.
     const unhook = intercept((text) => {
-      
+
       // Make sure some text has been captures.
       if( text ) {
-      
+
         // Get next index in result set.
         let next = results.length;
 
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
 
         // Make sure the item has been initialized.
         if( !Array.isArray(results[next]) ) results[next] = [];
-        
+
         // Check for types.
         if( text.indexOf('SUCCESS') === 0 ) type = 'success';
         if( text.indexOf('ERROR') === 0 ) type = 'error';
@@ -67,23 +67,35 @@ module.exports = function (grunt) {
 
         // Capture line breaks.
         if( (/^\r?\n$/).test(text) ) results.push({type: type = 'log', text});
-          
+
         // Add to the array.
         else results[next].push({type, text});
-        
+
       }
-      
+
       // Discard original text.
       return '';
-      
+
     });
-  
+
+    // Initialize a custom logger.
+    const logger = (text) => process.stdout.write(text);
+
+    // Standardize our logging methods.
+    const log = {
+      log: logger,
+      success: logger,
+      error: logger,
+      warn: logger,
+      debug: logger
+    };
+
     // Wait for the Sass files to compile.
     await Promise.all(this.files.map(async (file) => {
-      
+
       // Attempt to compile the Sass file.
       try {
-      
+
         // Capture the source.
         const [src] = file.src;
 
@@ -94,29 +106,26 @@ module.exports = function (grunt) {
         const result = sass.renderSync(extend({}, options, {
           file: src,
           outFile: file.dest
-        })); 
-        
+        }));
+
         // Save the CSS file.
         grunt.file.write(file.dest, result.css);
-        
+
         // Save the source map.
         if( options.sourceMap ) grunt.file.write((options.sourceMap === true ? `${file.dest}.map` : options.sourceMap), result.map);
-        
+
         // Tally success.
         ++compiled;
-  
-      } 
-      
+
+      }
+
       // Catch any errors.
       catch(error) { throw error; }
-      
+
     })).then(() => {
-    
+
       // Unhook the interceptor.
       unhook();
-
-      // Standardize the console logger.
-      console.log = console.debug = console.error = console.warn = function(text) { process.stdout.write(text); };
 
       // Flatten the result set.
       results = results.reduce((array, result) => {
@@ -142,30 +151,30 @@ module.exports = function (grunt) {
           if( ['WARNING', 'ERROR', 'DEBUG'].includes(result.text) ) style.push('bold');
 
           // Output the result.
-          console[result.type](chalk`{${style.join('.')} ${result.text}}`);
+          log[result.type](chalk`{${style.join('.')} ${result.text}}`);
 
         }
 
         else console[result.type](result.text);
 
       });
-      
+
       // Report done.
-      console.log(chalk`{${styles.success.join('.')} Successfully compiled ${compiled} Sass file(s).\n}`);
+      log.log(chalk`{${styles.success.join('.')} Successfully compiled ${compiled} Sass file(s).\n}`);
 
       // Done.
       done();
 
-    }).catch((error) => { 
-      
+    }).catch((error) => {
+
       // Unhook the interceptor.
       unhook();
-      
+
       // Done, but errors occurred.
       done(error);
 
     });
-    
+
   });
-  
+
 };
